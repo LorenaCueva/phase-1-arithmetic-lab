4dbbc46fee44d3efb4d61dfe8f50b00c7fd1dc26
Lorena Cueva <lorenacueva@gmail.com>
2022-02-28 13:01:57 -0800

    commit tree 7b00e4cbb0bbf1550e32cd45066a43f67fe91709

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+const random = Number.parseInt(Math.random()*10);
+
+const num3 = 22;
+const num4 = 6;
+
+let mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,5,6,7,8,20);
